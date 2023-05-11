f8553933da0e41d010f14c917bfe86efc19e75ef
salurso <114727815+salurso@users.noreply.github.com>
2023-05-12 00:19:01 +0200

    Update LoginServlet.java

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -15,35 +15,20 @@ import java.security.NoSuchAlgorithmException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-
-        String address = "";
-
-        UtenteDAO utenteDAO = new UtenteDAO();
-        Utente utente = utenteDAO.doRetrieveByUsernamePassword(username, password);
-        if (utente!=null){   // the user is in the db then s/he is registered and can go on
-            // Salva l'utente nella sessione
-            HttpSession session = request.getSession();
-            session.setAttribute("utente", utente);
-            // Redirect alla home page dell'utente
-            address = "show-prodotti";
-        } else {
-            String verifica = "Email o password errati!";
-            request.setAttribute("verifica", verifica);
-            address = "/WEB-INF/results/login.jsp";
-            // Redirect alla pagina con un messaggio di errore
-            //address ="index.jsp?error=1";  // con redirect, la pagina non può essere in WEB-INF, il browser non potrebbe raggiungerla
+        String parametri;
+        HttpSession session = request.getSession();
+        Utente utente = UtenteDAO.doRetrieveByUsernamePassword(request.getParameter("username"), request.getParameter("password"));
+        if (request.getParameter("action") == null) {
+            if (utente == null) {
+                parametri = "Email o password errati!";
+                request.setAttribute("parametri", parametri);
+                RequestDispatcher rs = request.getRequestDispatcher("/WEB-INF/results/Login.jsp");
+                rs.include(request, response);
+            }
         }
-
-        //response.sendRedirect(address);
-        RequestDispatcher ds = request.getRequestDispatcher(address);
-        ds.forward(request, response);
     }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
-
 }
