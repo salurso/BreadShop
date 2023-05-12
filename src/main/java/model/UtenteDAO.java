9eeaba613f0bed6da7be63ffd7fcfdcc76504703
Paci-h24 <costa.paciello@gmail.com>
2023-05-12 14:02:01 +0200

    Update UtenteDAO.java

--- src/main/java/model/UtenteDAO.java
+++ src/main/java/model/UtenteDAO.java
@@ -3,29 +3,69 @@ package model;
 import java.sql.*;
 
 public class UtenteDAO {
-    public static Utente doRetrieveByUsernamePassword(String username, String password) {
+//    public static Utente doRetrieveByUsernamePassword(String username, String password) {
+//
+//        PreparedStatement statement = null;
+//        ResultSet resultSet = null;
+//        Utente utente = null;
+//
+//        if(username==null || password==null)
+//            return utente;
+//
+//        try (Connection connection = ConPool.getConnection()) {
+//            statement = connection.prepareStatement("SELECT  FROM utente WHERE username=? AND password=SHA1(?)");
+//            statement.setString(1, username);
+//            statement.setString(2, password);
+//            resultSet = statement.executeQuery();
+//
+//            if (resultSet.next()) {
+//                utente = new Utente();
+//                utente.setId(resultSet.getInt("id"));
+//                utente.setUsername(resultSet.getString("username"));
+//                utente.setPassword(password);
+//                utente.setName(resultSet.getString("name"));
+//                utente.setEmail(resultSet.getString("email"));
+//                utente.setAdmin(resultSet.getBoolean("admin"));
+//            }
+//        }  catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        } finally {
+//            try {
+//                if (resultSet != null) {
+//                    resultSet.close();
+//                }
+//                if (statement != null) {
+//                    statement.close();
+//                }
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//        return utente;
+//    }
+
+    public static Utente doRetrieveByEmailPassword(String email, String password) {
 
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         Utente utente = null;
 
-        if(username==null || password==null)
+        if(email==null || password==null)
             return utente;
 
         try (Connection connection = ConPool.getConnection()) {
-            statement = connection.prepareStatement("SELECT  FROM utente WHERE username=? AND password=SHA1(?)");
-            statement.setString(1, username);
+            statement = connection.prepareStatement("SELECT email, nome, cognome, password FROM utente WHERE email=? AND password=SHA1(?)");
+            statement.setString(1, email);
             statement.setString(2, password);
             resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
                 utente = new Utente();
-                utente.setId(resultSet.getInt("id"));
-                utente.setUsername(resultSet.getString("username"));
-                utente.setPassword(password);
-                utente.setName(resultSet.getString("name"));
                 utente.setEmail(resultSet.getString("email"));
-                utente.setAdmin(resultSet.getBoolean("admin"));
+                utente.setName(resultSet.getString("name"));
+                utente.setSurname(resultSet.getString("surname"));
+                utente.setPassword(password);
             }
         }  catch (SQLException e) {
             throw new RuntimeException(e);
@@ -45,7 +85,6 @@ public class UtenteDAO {
         return utente;
     }
 
-
 //    public void doSave(Utente utente) {
 //
 //            try (Connection con = ConPool.getConnection()) {
@@ -64,15 +103,33 @@ public class UtenteDAO {
 //            }
 //    }
 
+//    public static void doRegistration(Utente utente) {
+//        try (Connection con = ConPool.getConnection()) {
+//            PreparedStatement ps = con.prepareStatement("INSERT INTO utente (username, password, email, name, admin) VALUES (?,?,?,?,?)",
+//                    Statement.RETURN_GENERATED_KEYS);
+//            ps.setString(1, utente.getUsername());
+//            ps.setString(2, utente.getPassword());
+//            ps.setString(3, utente.getEmail());
+//            ps.setString(4, utente.getName());
+//            ps.setBoolean(5, false);
+//
+//            if (ps.executeUpdate() != 1)
+//                throw new RuntimeException("Errore nel definire l'utente");
+//        }
+//        catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
     public static void doRegistration(Utente utente) {
         try (Connection con = ConPool.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("INSERT INTO utente (username, password, email, name, admin) VALUES (?,?,?,?,?)",
+            PreparedStatement ps = con.prepareStatement("INSERT INTO utente (email, nome, cognome, admin, password) VALUES (?,?,?,?,?)",
                     Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, utente.getUsername());
-            ps.setString(2, utente.getPassword());
-            ps.setString(3, utente.getEmail());
-            ps.setString(4, utente.getName());
-            ps.setBoolean(5, false);
+            ps.setString(1, utente.getEmail());
+            ps.setString(2, utente.getName());
+            ps.setString(3, utente.getSurname());
+            ps.setBoolean(4, false);
+            ps.setString(5, utente.getPassword());
 
             if (ps.executeUpdate() != 1)
                 throw new RuntimeException("Errore nel definire l'utente");
