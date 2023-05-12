b99b8933382aae8d5978a9b43ee266f1627c2a4e
Paci-h24 <costa.paciello@gmail.com>
2023-05-12 14:01:57 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -4,33 +4,61 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 public class Utente {
-    public String getName() {
-        return name;
+
+//    private int id;
+    private String email;
+    private String name;
+    private String surname;
+    private String username;
+    private int phone_number;
+    private String city;
+    private String street;
+    private int street_number;
+    private String province;
+    private int cap;
+    private boolean admin;
+    private String password;
+
+
+    // metodi getter e setter
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    private int id;
     public void setName(String name) {
         this.name = name;
     }
 
-    private String name;
-    private String username;
-    private String password;
-    private String email;
-    private boolean admin;
-
-
-    // metodi getter e setter
-    public String getUsername() {
-        return username;
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
     }
 
-    public String getPassword() {
-        return password;
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setStreet_number(int street_number) {
+        this.street_number = street_number;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public void setCap(int cap) {
+        this.cap = cap;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
     }
 
     public void setPassword(String password) {
@@ -45,28 +73,71 @@ public class Utente {
         }
     }
 
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+
+
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getStreet_number() {
+        return street_number;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public int getCap() {
+        return cap;
     }
 
     public boolean isAdmin() {
         return admin;
     }
 
-    public void setAdmin(boolean admin) {
-        this.admin = admin;
+    public String getPassword() {
+        return password;
     }
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+
+
+
+
+
+//    public int getId() {
+//        return id;
+//    }
+
+//    public void setId(int id) {
+//        this.id = id;
+//    }
 }
 
