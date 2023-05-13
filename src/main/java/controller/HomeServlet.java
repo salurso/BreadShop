0b5f2e8b57d9a28d64b9a5c19d449dbbbb9d6bda
salurso <114727815+salurso@users.noreply.github.com>
2023-05-13 18:51:31 +0200

    Update HomeServlet.java

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -6,13 +6,20 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Prodotto;
+import model.ProdottoDAO;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-@WebServlet("/Homepage")
-public class Homepage extends HttpServlet {
+@WebServlet("/HomePage")
+public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProdottoDAO pDAO = new ProdottoDAO();
+        ArrayList<Prodotto> prodotti = new ArrayList<Prodotto>();
+        prodotti = (ArrayList<Prodotto>) pDAO.doRetrieveAll();
+        request.setAttribute("prodotti", prodotti);
         RequestDispatcher ds = request.getRequestDispatcher("index.jsp");
         ds.forward(request, response);
     }
