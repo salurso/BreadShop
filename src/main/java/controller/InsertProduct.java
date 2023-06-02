bf95685f668b74b4a8e2eeeca6ca951fe89148c1
Paci-h24 <costa.paciello@gmail.com>
2023-06-02 21:24:42 +0200

    Update InsertProduct.java

--- src/main/java/controller/InsertProduct.java
+++ src/main/java/controller/InsertProduct.java
@@ -13,29 +13,63 @@ import model.ProdottoDAO;
 import model.Utente;
 import model.UtenteDAO;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @MultipartConfig
 @WebServlet(name = "InsertProduct", value = "/InsertProduct")
 public class InsertProduct extends HttpServlet {
+    private static final String CARTELLA_UPLOAD = "upload";
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = "PANE";
-        Double prezzo = 2.5;
-        String descrizione = "prova inserimento";
+        String name = request.getParameter("name");
+        Double price = Double.valueOf(request.getParameter("price"));
+        String description = request.getParameter("description");
+        String category = request.getParameter("category");
 
-        Part part = request.getPart("image");
-        InputStream in = part.getInputStream();
+//        Part part = request.getPart("image");
+//        InputStream in = part.getInputStream();
+
+        Part filePart = request.getPart("image");
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
 
         Prodotto p = new Prodotto();
         p.setName(name);
-        p.setPrice(prezzo);
-        p.setImage(in);
-        p.setDescription(descrizione);
+        p.setPrice(price);
+//        p.setImage(in);
+        p.setImage(fileName);
+        p.setDescription(description);
+        p.setNameCategory(category);
 
         ProdottoDAO pDAO = new ProdottoDAO();
         pDAO.doInsertProduct(p);
+
+
+
+        String destinazione = CARTELLA_UPLOAD + File.separator + fileName;
+        Path pathDestinazione = Paths.get(getServletContext().getRealPath(destinazione));
+
+        // se un file con quel nome esiste già, gli cambia nome
+        for (int i = 2; Files.exists(pathDestinazione); i++) {
+            destinazione = CARTELLA_UPLOAD + File.separator + i + "_" + fileName;
+            pathDestinazione = Paths.get(getServletContext().getRealPath(destinazione));
+        }
+
+        InputStream fileInputStream = filePart.getInputStream();
+        // crea CARTELLA_UPLOAD, se non esiste
+        Files.createDirectories(pathDestinazione.getParent());
+        // scrive il file
+        Files.copy(fileInputStream, pathDestinazione);
+
+        request.setAttribute("uploaded", fileName);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/administrator/uploadResult.jsp");
+        requestDispatcher.forward(request, response);
 
     }
 
