6bb02a5d6ccc40aabf23765ab83707ede67fa471
Paci-h24 <costa.paciello@gmail.com>
2023-06-02 21:24:49 +0200

    Update ProdottoDAO.java

--- src/main/java/model/ProdottoDAO.java
+++ src/main/java/model/ProdottoDAO.java
@@ -16,7 +16,7 @@ public class ProdottoDAO {
 
         try (Connection con = ConPool.getConnection()) {
 
-            PreparedStatement ps = con.prepareStatement("select id, nome, prezzo from prodotto");
+            PreparedStatement ps = con.prepareStatement("select id, nome, prezzo, immagine, nomeCategoria from prodotto");
             ResultSet rs = ps.executeQuery();
             ArrayList<Prodotto> prodotti = new ArrayList<>();
 
@@ -24,9 +24,9 @@ public class ProdottoDAO {
                 Prodotto p = new Prodotto();
                 p.setId(rs.getInt(1));
                 p.setName(rs.getString(2));
-//                p.setDescription(rs.getString(3));
                 p.setPrice(rs.getDouble(3));
-//                p.setImage(rs.getByte(4));
+                p.setImage(rs.getString(4));
+                p.setNameCategory(rs.getString(5));
 
                 prodotti.add(p);
             }
@@ -42,44 +42,25 @@ public class ProdottoDAO {
         int result;
             try (Connection con = ConPool.getConnection()) {
 
-                PreparedStatement ps = con.prepareStatement("INSERT INTO prodotto (nome, prezzo, immagine, descrizione, idCategoria) VALUES (?, ?, ?, ?, ?)");
+                PreparedStatement ps = con.prepareStatement("INSERT INTO prodotto (nome, prezzo, immagine, descrizione, nomeCategoria) VALUES (?, ?, ?, ?, ?)");
                 ps.setString(1, p.getName());
                 ps.setDouble(2, p.getPrice());
-                ps.setBlob(3, p.getImage());
+//                ps.setBlob(3, p.getImage());
+                ps.setString(3,p.getImage());
                 ps.setString(4, p.getDescription());
-                ps.setInt(5, 1);
+                ps.setString(5, p.getNameCategory());
 
                 result = ps.executeUpdate();
-
             } catch (SQLException s) {
                 throw new RuntimeException(s);
             }
         return false;
     }
 
-//    String sql = INSERT INTO user VALUES (?, ?, ?)
-//    PreparedStatement statement = null;
-//
-//    Part part = req.getPart("image");
-//
-//if(part != null){
-//        try{
-//            Class.forName("com.mysql.jdbc.Driver");
-//            Connection con = db.getConnection();
-//
-//            PreparedStatement ps = con.preparedStatement("insert
-//                    InputStream is = part.getInputStream();
-//            ps.setBlob(1, is);
-//            result = ps.executeUpdate();
-//        }
-//        catch(Exception e){
-//            e.printSTackTrace();
-//        }
-
     public List<Prodotto> doRetrieveByCategory(String cat) {
 
         try (Connection con = ConPool.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("SELECT id, nome, descrizione, prezzo FROM Prodotto p, Categoria c WHERE p.nomeCategoria = c.nome AND c.nome = ?");
+            PreparedStatement ps = con.prepareStatement("SELECT id, nome, prezzo, immagine FROM Prodotto  WHERE nomeCategoria = ?");
 
             ResultSet rs = ps.executeQuery();
             List<Prodotto> prodotti = new ArrayList<>();
@@ -87,8 +68,8 @@ public class ProdottoDAO {
                 Prodotto p = new Prodotto();
                 p.setId(rs.getInt(1));
                 p.setName(rs.getString(2));
-                p.setDescription(rs.getString(3));
-                p.setPrice(rs.getDouble(4));
+                p.setPrice(rs.getDouble(3));
+                p.setImage(rs.getString(4));
 
                 prodotti.add(p);
             }
@@ -101,15 +82,16 @@ public class ProdottoDAO {
     public Prodotto doRetrieveById(int id) {
 
         try (Connection con = ConPool.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("SELECT id, nome, descrizione, prezzo FROM Prodotto WHERE id = ?");
+            PreparedStatement ps = con.prepareStatement("SELECT id, nome, prezzo, immagine FROM Prodotto WHERE id = ?");
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
 
             Prodotto p = new Prodotto();
             p.setId(rs.getInt(1));
             p.setName(rs.getString(2));
-            p.setDescription(rs.getString(3));
-            p.setPrice(rs.getDouble(4));
+//            p.setDescription(rs.getString(3));
+            p.setPrice(rs.getDouble(3));
+            p.setImage(rs.getString(4));
 
             return p;
         } catch (SQLException s) {
@@ -117,29 +99,29 @@ public class ProdottoDAO {
         }
     }
 
-    public Prodotto doRetrieveByIdImage(int id) {
-
-        try (Connection con = ConPool.getConnection()) {
-            PreparedStatement ps = con.prepareStatement("SELECT id, nome, descrizione, prezzo, immagine FROM Prodotto WHERE id = ?");
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-//            Blob blob = rs.getBlob("immagine");
-//            byte byteArray[] = blob.getBytes(1, (int) blob.length());
-
-            Prodotto p = new Prodotto();
-            p.setId(rs.getInt("id"));
-            p.setName(rs.getString("nome"));
-            p.setDescription(rs.getString("descrizione"));
-            p.setPrice(rs.getDouble("prezzo"));
-//            p.setImage((InputStream) blob);
-            InputStream is = rs.getBinaryStream("immagine");
-            p.setImage(is);
-//            p.setImage((InputStream) rs.getBlob("immagine"));
-
-            return p;
-        } catch (SQLException s) {
-            throw new RuntimeException(s);
-        }
-    }
+//    public Prodotto doRetrieveByIdImage(int id) {
+//
+//        try (Connection con = ConPool.getConnection()) {
+//            PreparedStatement ps = con.prepareStatement("SELECT id, nome, descrizione, prezzo, immagine FROM Prodotto WHERE id = ?");
+//            ps.setInt(1, id);
+//            ResultSet rs = ps.executeQuery();
+////            Blob blob = rs.getBlob("immagine");
+////            byte byteArray[] = blob.getBytes(1, (int) blob.length());
+//
+//            Prodotto p = new Prodotto();
+//            p.setId(rs.getInt("id"));
+//            p.setName(rs.getString("nome"));
+//            p.setDescription(rs.getString("descrizione"));
+//            p.setPrice(rs.getDouble("prezzo"));
+////            p.setImage((InputStream) blob);
+//            InputStream is = rs.getBinaryStream("immagine");
+//            p.setImage(is);
+////            p.setImage((InputStream) rs.getBlob("immagine"));
+//
+//            return p;
+//        } catch (SQLException s) {
+//            throw new RuntimeException(s);
+//        }
+//    }
 
 }
